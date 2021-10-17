5ff2197db133997ef715a53e2bf87a86f350361f
coscoaj1 <coscoanthony1@gmail.com>
2021-10-17 14:31:22 -0400

    updated mongoose schema

--- models/home.js
+++ models/home.js
@@ -1,33 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const homeSchema = new mongoose.Schema({
-	description: {
-		type: String,
-		minLength: 5,
-		required: true,
-	},
+  description: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
 
-	owner: {
-		type: String,
-		required: true,
-	},
+  title: {
+    type: String,
+    minLength: 5,
+  },
 
-	rating: {
-		type: Number,
-	},
+  price: {
+    type: Number,
+    minLength: 2,
+  },
 
-	pictureUrl: {
-		type: String,
-		required: true,
-	},
+  bedrooms: {
+    type: Number,
+    minLength: 1,
+  },
+  beds: {
+    type: Number,
+    minLength: 1,
+  },
+  baths: {
+    type: Number,
+    minLength: 1,
+  },
+
+  kitchens: {
+    type: Number,
+    minLength: 1,
+  },
+
+  owner: {
+    type: String,
+    required: true,
+  },
+
+  rating: {
+    type: Number,
+  },
+
+  pictureUrl: {
+    type: String,
+    required: true,
+  },
 });
 
-homeSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+homeSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
-module.exports = mongoose.model('Home', homeSchema);
+module.exports = mongoose.model("Home", homeSchema);
