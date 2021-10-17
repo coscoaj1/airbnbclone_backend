b493a1a523ae4487b3f68a803ed0ac533c7ccc82
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-17 18:19:56 -0400

    updated mongoose model

--- models/home.js
+++ models/home.js
@@ -1,61 +1,68 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const homeSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    minLength: 5,
-    required: true,
-  },
+	description: {
+		type: String,
+		minLength: 5,
+		required: true,
+	},
 
-  title: {
-    type: String,
-    minLength: 5,
-  },
+	title: {
+		type: String,
+		minLength: 5,
+	},
 
-  price: {
-    type: Number,
-    minLength: 2,
-  },
+	price: {
+		type: Number,
+		minLength: 2,
+	},
 
-  bedrooms: {
-    type: Number,
-    minLength: 1,
-  },
-  beds: {
-    type: Number,
-    minLength: 1,
-  },
-  baths: {
-    type: Number,
-    minLength: 1,
-  },
+	bedrooms: {
+		type: Number,
+		minLength: 1,
+	},
+	beds: {
+		type: Number,
+		minLength: 1,
+	},
+	baths: {
+		type: Number,
+		minLength: 1,
+	},
 
-  kitchens: {
-    type: Number,
-    minLength: 1,
-  },
+	kitchens: {
+		type: Number,
+		minLength: 1,
+	},
 
-  owner: {
-    type: String,
-    required: true,
-  },
+	owner: {
+		type: String,
+		required: true,
+	},
 
-  rating: {
-    type: Number,
-  },
+	rating: {
+		type: Number,
+	},
 
-  pictureUrl: {
-    type: String,
-    required: true,
-  },
+	pictureUrl: {
+		type: String,
+		required: true,
+	},
+	guests: {
+		type: Number,
+		required: true,
+	},
+	reviews: {
+		type: Number,
+	},
 });
 
-homeSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+homeSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
 });
 
-module.exports = mongoose.model("Home", homeSchema);
+module.exports = mongoose.model('Home', homeSchema);
