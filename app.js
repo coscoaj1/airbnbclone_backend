c501e4dc09b03e623ef451945db1ca9eb4741cdd
coscoaj1 <coscoanthony1@gmail.com>
2021-10-24 13:23:55 -0400

    npm fix

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const config = require('./utils/config');
-const express = require('express');
+const config = require("./utils/config");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const homesRouter = require('./controllers/homes');
-const logger = require('./utils/logger');
-const mongoose = require('mongoose');
-const requestLogger = require('./utils/middleware');
-const errorHandler = require('./utils/middleware');
+const morgan = require("morgan");
+const cors = require("cors");
+const homesRouter = require("./controllers/homes");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const requestLogger = require("./utils/middleware");
 
-logger.info('connecting to', config.MONGODB_URI);
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
-	.connect(config.MONGODB_URI)
-	.then((result) => {
-		console.log('connected to MongoDB');
-	})
-	.catch((error) => {
-		console.log('error connecting to MongoDB:', error.message);
-	});
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(requestLogger);
 
-app.use('/api/homes', homesRouter);
+app.use("/api/homes", homesRouter);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const app = require('./app');
-const http = require('http');
-const logger = require('./utils/logger');
-const config = require('./utils/config');
+const app = require("./app");
+const http = require("http");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const server = http.createServer(app);
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' });
+  response.status(404).send({ error: "unknown endpoint" });
 };
 
 const PORT = 3001;
 server.listen(PORT, () => {
-	logger.info(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
